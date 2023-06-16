61291f98d00b81268d98cab0826e27d31d99d9a6
William Blackman <57603228+william-blackman@users.noreply.github.com>
2023-06-16 16:47:21 -0700

    Added index.html and main.js

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+window.alert('Hello, World!');  //Opens a popup window to say Hello, world!
+
+document.write('Hello, world!  ');  //Prints Hello, world in the browser
+
+var var1 = "This is a string with a quote: \"I had a dream\"." + "--Martin Luther King"; var2 = "The King"; // strings assigned to variables
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string. "; //more strings assinged to variables
+
+document.write(Sent1 + Sent2); //concatenates Sent1 and Sent 2 and displays them in the browser
+
+var x=12, y=4;  //integers assigned to variables
+function add (a,b) { //adding function
+    return a + b
+};
+
+
+document.write(add(x,y)); //calls add function to add a & b
